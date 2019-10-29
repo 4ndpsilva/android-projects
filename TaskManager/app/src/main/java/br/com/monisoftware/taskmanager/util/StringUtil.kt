81cc1c0b529ab9227e6ba70bc21371ee199a9749
André <4ndpsilva@gmail.com>
2019-10-29 02:20:18 -0300

    Ajuste na expressão regular

--- TaskManager/app/src/main/java/br/com/monisoftware/taskmanager/util/StringUtil.kt
+++ TaskManager/app/src/main/java/br/com/monisoftware/taskmanager/util/StringUtil.kt
@@ -2,6 +2,6 @@ package br.com.monisoftware.taskmanager.util
 
 object StringUtil{
     fun isLetterOrDigitOnly(value: String): Boolean{
-        return value.contains("^[a-zA-Z0-9]+$".toRegex())
+        return value.contains("^[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ0-9\\s+]+$".toRegex())
     }
 }
