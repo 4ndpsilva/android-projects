e00e054fc8b07d7a57018e4299be8dd0d7a1a564
4ndpsilva <4ndpsilva@gmail.com>
2019-11-08 09:47:25 -0300

    Remoção do projeto antigo

--- TaskManager/app/src/main/java/br/com/monisoftware/tasklist/util/StringUtil.kt
+++
@@ -1,7 +0,0 @@
-package br.com.monisoftware.taskmanager.util
-
-object StringUtil{
-    fun isLetterOrDigitOnly(value: String): Boolean{
-        return value.contains(Constants.REGEX_LETTER_DIGIT.toRegex())
-    }
-}
--- TaskManager/app/src/main/java/br/com/monisoftware/tasklist/view/BaseView.kt
+++
@@ -1,5 +0,0 @@
-package br.com.monisoftware.taskmanager.view
-
-interface BaseView<T>{
-    fun setPresenter(presenter: T)
-}
