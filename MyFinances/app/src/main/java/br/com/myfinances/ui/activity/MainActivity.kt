cf213ab1af4240917da0c80babf55ece45db5893
André <4ndpsilva@gmail.com>
2019-11-21 04:02:40 -0300

    ajustes no Creator

--- MyFinances/app/src/main/java/br/com/myfinances/ui/activity/MainActivity.kt
+++ MyFinances/app/src/main/java/br/com/myfinances/ui/activity/MainActivity.kt
@@ -8,8 +8,8 @@ import br.com.myfinances.viewmodel.CategoryViewModel
 import br.com.myfinances.viewmodel.Creator
 
 class MainActivity : AppCompatActivity(){
-    private val categoryVM: CategoryViewModel by viewModels { Creator.viewModelCategory(this) }
-    private val accountVM: AccountViewModel by viewModels { Creator.viewModelAccount(this) }
+    private val categoryVM: CategoryViewModel by viewModels { Creator.vmFactoryCategory(this) }
+    private val accountVM: AccountViewModel by viewModels { Creator.vmFactoryAccount(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- MyFinances/app/src/main/java/br/com/myfinances/viewmodel/Creator.kt
+++ MyFinances/app/src/main/java/br/com/myfinances/viewmodel/Creator.kt
@@ -7,19 +7,19 @@ import br.com.myfinances.data.repository.CategoryRepository
 import br.com.myfinances.data.repository.EntryRepository
 
 object Creator {
-    fun viewModelCategory(cxt: Context) : ViewModelFactory<CategoryRepository>{
+    fun vmFactoryCategory(cxt: Context) : ViewModelFactory<CategoryRepository>{
         val dao = DatabaseApp(cxt).categoryDAO()
         val repo = CategoryRepository(dao)
         return ViewModelFactory(repo)
     }
 
-    fun viewModelAccount(cxt: Context) : ViewModelFactory<AccountRepository>{
+    fun vmFactoryAccount(cxt: Context) : ViewModelFactory<AccountRepository>{
         val dao = DatabaseApp(cxt).accountDAO()
         val repo = AccountRepository(dao)
         return ViewModelFactory(repo)
     }
 
-    fun viewModelEntry(cxt: Context) : ViewModelFactory<EntryRepository>{
+    fun vmFactoryEntry(cxt: Context) : ViewModelFactory<EntryRepository>{
         val dao = DatabaseApp(cxt).entryDAO()
         val repo = EntryRepository(dao)
         return ViewModelFactory(repo)
