cd26fb146441e557ad69b0085f473d1dd2bc4b41
André <4ndpsilva@gmail.com>
2020-01-03 03:16:40 -0300

    Ajustes da toolbar

---
+++ MyNotes/app/src/main/java/br/com/mynotes/App.kt
@@ -0,0 +1,9 @@
+package br.com.mynotes
+
+import android.app.Application
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- MyNotes/app/src/main/java/br/com/mynotes/ui/activity/MainActivity.kt
+++ MyNotes/app/src/main/java/br/com/mynotes/ui/activity/MainActivity.kt
@@ -1,12 +1,31 @@
 package br.com.mynotes.ui.activity
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import br.com.mynotes.R
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        setSupportActionBar(main_toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.add){
+            Toast.makeText(this, "clicado", Toast.LENGTH_SHORT).show()
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
